c6427d77f6a3c95b1928a84a2af1871d9c2ff6c2
CL1-PC15 <CL1-PC15@DESKTOP-VNOBLCE>
2025-07-18 09:16:10 +0800

    SAMPLE PROGRAM ACT2B

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author CL1-PC15
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("SUMAWANG DUSTINE");
+        System.out.println("ACT 2B");
+    }
+}
